eba91b4af769b058a1517610d49b257e28fd3c6f
Gustavo Rosa <g.dsantosrosa@gmail.com>
2024-09-10 14:15:05 -0300

    feat: Implementacao disabled botao

--- scripts.js
+++ scripts.js
@@ -4,63 +4,63 @@ var products = [
         "name": "Big Mac",
         "price": 5.99,
         "active": false,
-        "quantity": 1
+        "quantity": 0
     },
     {
         "photo": "img/mc-chicken.png",
         "name": "Mc Chicken",
         "price": 4.99,
         "active": false,
-        "quantity": 1
+        "quantity": 0
     },
     {
         "photo": "img/double-cb.png",
         "name": "Double Cheese Burger",
         "price": 2.99,
         "active": false,
-        "quantity": 1
+        "quantity": 0
     },
     {
         "photo": "img/fries.png",
         "name": "Batata frita",
         "price": 2.99,
         "active": false,
-        "quantity": 1
+        "quantity": 0
     },
     {
         "photo": "img/nuggets.png",
         "name": "Mc Nuggets",
         "price": 3.49,
         "active": false,
-        "quantity": 1
+        "quantity": 0
     },
     {
         "photo": "img/salad.png",
         "name": "Salada",
         "price": 2.79,
         "active": false,
-        "quantity": 1
+        "quantity": 0
     },
     {
         "photo": "img/cola.png",
         "name": "Coca Cola",
-        "price": 1.99,
+        "price": 0.99,
         "active": false,
-        "quantity": 1
+        "quantity": 0
     },
     {
         "photo": "img/lipton.png",
         "name": "Ice Tea",
-        "price": 1.99,
+        "price": 0.99,
         "active": false,
-        "quantity": 1
+        "quantity": 0
     },
     {
         "photo": "img/water.png",
         "name": "Água",
-        "price": 1.49,
+        "price": 0.49,
         "active": false,
-        "quantity": 1
+        "quantity": 0
     }
 ];
 
